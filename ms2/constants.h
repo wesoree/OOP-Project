cde1ff2dde5b7d574569469228853727ebb706ef
Fardad Soleimanloo <fardad.soleimanloo@senecapolytechnic.ca>
2025-05-04 10:58:14 -0400

    2254 init

--- ms2/constants.h
+++
@@ -1,6 +0,0 @@
-#ifndef SENECA_CONSTANTS_H
-#define SENECA_CONSTANTS_H
-namespace seneca {
-   const size_t MaximumNumberOfMenuItems = 20u;
-}
-#endif // !SENECA_CONSTANTS_H
--- ms3/constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef SENECA_CONSTANTS_H
-#define SENECA_CONSTANTS_H
-
-namespace seneca {
-   const size_t MaximumNumberOfMenuItems = 20u;
-}
-
-#endif // !SENECA_CONSTANTS_H
